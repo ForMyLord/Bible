fcbac1bb8d40ec00ab40b0038d61d42ef8df0a49
sangyun5108 <tkfkd4447@naver.com>
2022-08-29 17:13:50 +0900

    성경(창세기) 구현하는 중

--- lib/View/bible.dart
+++ lib/View/bible.dart
@@ -1,24 +1,49 @@
+import 'package:bible/Model/bible_list.dart';
 import 'package:flutter/material.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class Bible extends StatefulWidget {
-  Bible(this.title, {super.key});
 
-  final String title;
+  final int bibleChapter;
+  final int biblePassenger;
+  final String bibleTitle;
+  final Map<String,Map<String,String>> bible = genesis;
+
+  Bible({required this.bibleChapter, required this.biblePassenger, required this.bibleTitle});
 
   @override
   State<Bible> createState() => _BibleState();
 }
 
 class _BibleState extends State<Bible> {
+
+  final ItemScrollController itemScrollController = ItemScrollController();
+
+
   @override
   Widget build(BuildContext context) {
+
+    double c_width = MediaQuery.of(context).size.width*0.95;
+
+    print(widget.bible["1장"]?.length);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("${widget.title}"),
+        title: Text("${widget.bibleTitle} ${widget.bibleChapter+1} 장 ${widget.biblePassenger+1} 절"),
       ),
-      body: Center(
-        child: Text("${widget.title}"),
-      ),
-    );
+      body: SafeArea(
+        child: Container(
+          width: c_width,
+            child: ScrollablePositionedList.builder(itemCount: widget.bible["1장"]!.length, itemBuilder: (context,index){
+              return Container(
+                padding: const EdgeInsets.fromLTRB(20, 10, 0, 0),
+                child: Text("${index+1}절  ${widget.bible["1장"]!["${index+1}절"]}",style: const TextStyle(
+                    fontSize: 20
+                ),textAlign: TextAlign.left),
+              );
+            }),
+          ),
+        ),
+      );
   }
 }
--- lib/View/home.dart
+++ lib/View/home.dart
@@ -11,11 +11,36 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
 
-  String a = "창세기";
+  String bibleTitle = "창세기";
 
-  String searchText = "";
-  final TextEditingController _textController = TextEditingController();
-  bool showSearchPreview = false;
+  String searchText = ""; // 검색 텍스트
+  final TextEditingController _textController = TextEditingController(); // TextController 만들기
+
+  bool showSearchPreview = false; // 연관 검색어 보여주는데 필요한 bool 값
+
+  int chapterLength = 50; // 말씀 장 길이
+
+  List<int> bibleChapter = []; // Card로 찍어내기 위한 배열 선언
+  List<int> biblePassage = [];
+
+  int bibleLength = 50; // 장 길이
+  int passageLength = 20;
+
+  int sendBibleChapter = 0;
+
+  bool setVerse = false; // 다이얼로그 장에서 절로 넘어가는지 확인
+
+  getChapter(){
+    for(int i = 1; i <= chapterLength ; i++){
+      bibleChapter.add(i);
+    }
+  }
+
+  @override
+  void initState() {
+    getChapter();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,16 +99,52 @@ class _HomepageState extends State<Homepage> {
                       child: ListView.builder(
                           itemCount: bible_list.length,
                           itemBuilder: (BuildContext context, int index){
-                        return Container(
-                          height: 20,
-                          child: Center(
-                            child: GestureDetector(
-                              child: Text('${bible_list[  index]}'),
+                        return Center(
+                          child: GestureDetector(
+                              child: Text('${bible_list[index]}'),
                               onTap: (){
-                                print("${bible_list[index]}가 클릭됨");
-                                Navigator.push(context,MaterialPageRoute(builder: (context) => Bible("${bible_list[index]}")));
-                              },
-                            ),
+                                bibleTitle = bible_list[index];
+                                showDialog(context: context, builder: (BuildContext context){
+                                  return Dialog( // 장을 선택할 수 있는 Dialog
+                                    child:GridView.extent(
+                                      maxCrossAxisExtent: 60,
+                                      children: [
+                                        for(int i = 0; i < bibleLength; i++)  GestureDetector(
+                                          child: Card(
+                                            elevation: 2.0,
+                                            child: Center(
+                                                child: Text('${bibleChapter[i]}장')
+                                            ),
+                                          ),
+                                          onTap: (){
+                                            setState((){
+                                              setVerse = true;
+                                            });
+                                            showDialog(context: context, builder: (BuildContext context){
+                                              return Dialog( // 절을 선택할 수 있는 Dialog
+                                                child: GridView.extent(maxCrossAxisExtent: 60,
+                                                  children: [
+                                                    for(int j = 0; j < bibleLength; j++)  GestureDetector(
+                                                      child: Card(
+                                                        elevation: 2.0,
+                                                        child: Center(
+                                                            child: Text('${bibleChapter[j]}절')
+                                                        ),
+                                                      ),
+                                                      onTap: (){
+                                                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => Bible(bibleChapter: i, biblePassenger: j,bibleTitle: bibleTitle,)));
+                                                      },
+                                                    )
+                                                  ],),
+                                              );
+                                            });
+                                          },
+                                        )
+                                      ],
+                                    ),
+                                  );
+                                });
+                              }
                           ),
                         );
                       }),
